6abe58fbe0db72ffdd465bcf880edf582e6ff750
ayush sharma <ayush89able@gmail.com>
2020-03-27 01:46:55 +0530

    change in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,10 @@ import Text from '@tds/core-text'
 const Navbar = props => {
     return (
         <Box style={{ background: '#4b286d', width: '100%', top: '0', left: '0', position: 'fixed', zIndex: '999' }} vertical={2} between={2}>
-            <div style={{ display: 'flex', justifyContent: "space-between", zIndex: '100' }}>
+            <div style={{ display: 'inline', zIndex: '100' }}>
                 <Text invert="false">Practice Project</Text>
-                <Link style={{ color: '#FFFFFF' }} to='/login'>LOGIN</Link>
-                <Link style={{ color: '#FFFFFF' }} to='/register'>REGISTER</Link>
+                <Link style={{ color: '#FFFFFF', float: 'right', marginRight: '1em' }} to='/login'>LOGIN</Link>
+                <Link style={{ color: '#FFFFFF', float: 'right', marginRight: '1em' }} to='/register'>REGISTER</Link>
             </div>
         </Box>
     )
